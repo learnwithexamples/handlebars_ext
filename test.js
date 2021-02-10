258914dbb68fdb2463acd5ca3c881ad544b1010f
Learn with Examples <learnwithexamples@outlook.com>
2021-02-09 22:17:29 -0800

    update

--- test.js
+++ test.js
@@ -6,19 +6,17 @@ handlebars.registerPartial('double', s => s + s); // register more partials/help
 const tpl0 = compile('compile template from string. double({{str}}) is {{> double str}}'); // compile template from string
 const tpl1 = compileFile('./test/test1.hbs'); // compile template from file
 
-// console.log(parse({ str: "hello Handlebars!" }, tpl0)); // parse string to string
-// console.log(parse({ str: "hello Handlebars!" }, tpl0, null, s => s + s + s)); // parse string to string, use a postProc function to duplicate the result 3 times
+console.log(parse({ str: "hello Handlebars!" }, tpl0)); // parse string to string
+console.log(parse({ str: "hello Handlebars!" }, tpl0, null, s => s + s + s)); // parse string to string, use a postProc function to duplicate the result 3 times
 
-// parseFile('./test/test1.json', tpl1, './test/test1.txt'); // parse file to file and string
+parseFile('./test/test1.json', tpl1, './test/test1.txt'); // parse file to file and string
 
-// console.log(readFormatJson('./test/test1.json')); // load and format json file
+console.log(readFormatJson('./test/test1.json')); // load and format json file
 
 const myfunc = (f, copyright = 'learn with examples @ 2021') => { // each json file, parse and add copyright
   if (f.endsWith('.json')) {
     const pt = path.parse(f);
-    parseFile(f, tpl1, path.join(pt.dir, pt.name + '.txt'), res => copyright + '\n' + res);  
+    parseFile(f, tpl1, path.join(pt.dir, pt.name + '.txt'), res => copyright + '\n\n' + res);  
   }
 };
-walkDir('./test', myfunc);
-// const sayHi = (f, str1 = "a", str2 = "b", str3 = "c") => { if (f.endsWith('json')) { console.log(f, str1, str2, str3); } } // your self-defined function
-// walkDir('.', sayHi, "xx", "yy"); // apply function to any file in a directory
+walkDir('./test', myfunc); // proc all files under test folder using myfunc
